f11d6bb24ef810a562946898185e70cf70aace5c
kishyda <marcuspyon@gmail.com>
2024-11-01 16:56:21 +0900

    Added a frontend client, as well as the foundations for the Spring Boot server

---
+++ webclient/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Board from './Board/Board'
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
